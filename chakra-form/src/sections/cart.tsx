8bb0a07e0e4d5ccf3ca5254186f848424aaae967
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-10-16 02:03:05 -0400

    light/dark mode switcher

--- chakra-form/src/sections/cart.tsx
+++ chakra-form/src/sections/cart.tsx
@@ -1,6 +1,21 @@
-import { VStack } from "@chakra-ui/react";
+import { 
+    VStack,
+    Heading,
+    HStack,
+    AspectRatio,
+    Text,
+    Button,
+    useColorMode,
+    useColorModeValue,
+    Image,
+    Stack,
+    Divider,
+} from "@chakra-ui/react";
 
 const Cart = () => {
+    const { toggleColorMode } = useColorMode();
+    const bgColor= useColorModeValue("gray.50", "whiteAlpha.50")
+    const secondaryTextColor = useColorModeValue("gray.600", "gray.400")
     return (
         <VStack
         w="full"
@@ -8,8 +23,59 @@ const Cart = () => {
         p={10}
         spacing={10}
         alignItems="flex-start"
-        bg="gray.50"
+        bg={bgColor}
         >
+            <VStack alignItems="flex-start" spacing={3}>
+                <Heading size="2xl">Your Cart</Heading>
+                <Text>if the price is too hard on your eyes, {
+                    <Button onClick={toggleColorMode} variant="link" colorScheme="black">
+                    try changing the theme.
+                </Button>
+                }
+                </Text>
+            </VStack>
+            <HStack spacing={6} alignItems="center" w="full">
+                <AspectRatio ratio={1} w={24}>
+                    <Image src="" alt="Question Mark" />
+                </AspectRatio>
+                <Stack 
+                spacing={0}
+                w="full"
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                >
+                    <VStack w="full" spacing={0} alignItems="flex-start">
+                        <Heading size="md">Penny Board</Heading>
+                        <Text color={secondaryTextColor}>PNYCOMP27541</Text>
+                    </VStack>
+                    <Heading size="md" textAlign="end">$119.00</Heading>
+                </Stack>
+            </HStack>
+            <VStack spacing={4} alignItems="stretch" w="full">
+
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Subtotal</Text>
+                    <Heading size="sm">$119.00</Heading>
+                </HStack>
+
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Shipping</Text>
+                    <Heading size="sm">$19.99</Heading>
+                </HStack>
+
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Taxes (estimated)</Text>
+                    <Heading size="sm">$23.80</Heading>
+                </HStack>
+            </VStack>
+            <Divider/>
+
+            <HStack justifyContent="space-between" w="full">
+                    <Text color={secondaryTextColor}>Subtotal</Text>
+                    <Heading size="lg">$162.79</Heading>
+            </HStack>
+            
         </VStack>
     )
 }
